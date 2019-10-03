8f902edb4fea81dfdab00d704890761e2dfc5b87
Francesco Leoni <fraleoni@gmail.com>
2019-10-03 08:01:10 +0200

    Work in progress...

--- Fatture/Codice.ts
+++ Fatture/Codice.ts
@@ -5,38 +5,57 @@ const debug = false;
 /** ID documenti  */
 const invoicesFileID = "1eqLHaMm9DFpofqGtETu8Mu7sUCak7plWHyRV37lbV0A";
 const invoiceTemplateID ="1i8VS32ksIrcUvJJOrn983xDgrE5qy9sbo4KfdPRODXs";
-const listinoID = "";
 /** altezza in px della finestra modale */
 const modalHeight = 600;
 /** Righe file fatture */
 const firstRow = 2;
 const lastRow = 70;
 const iRow = 22;
+// SEGNAPOSTO
+const RAGIONE_SOCIALE = "<RAGIONE_SOCIALE>";
+const INDIRIZZO = "<INDIRIZZO>";
+const CODICE_SAP = "<CODICE_SAP>";
+const CODICE_CIG = "<CODICE_CIG>";
+const NR_ORDINE = "<NR_ORDINE>";
+const UFF_VENDITE ="<UFF_VENDITE>"; //OLSE
+const DATA = "<DATA>";
+const EWBS = "<EWBS>"; //A200V01920C030120000_001 
 // RIFERIMENTI CELLE SHEET RICHIESTA FATTURA
-const CELL_ADDRESS = "D7";
+const CELL_ADDRESS = "E9";
 const CELL_DATA_1 = "G14";
 const CELL_DATA_2_COL = "E";
 const CELL_DATA_2_ROW = 24;
-const CELL_ORDER_NR_1 = "A14";
-const CELL_ORDER_NR_2 = "C14";
+const CELL_ORDER_NR_1 = "B14";
+const CELL_ORDER_NR_2 = "D14";
 const TOTAL_START_COL = 11;
 const QTY_START_COL = 9;
 const EXAMPLE_ROW = 21;
-const ORDERROW_START_COL = 2;
-const ORDERROW_COLS_NR = 10;
-// SEGNAPOSTO
-const INDIRIZZO = "<INDIRIZZO>";
-const NR_ORDINE = "<NR_ORDINE>";
-const DATA = "<DATA>";
-const NOME_RIFERIMENTO = "<NOME_RIFERIMENTO>";
-const EMAIL_RIFERIMENTO = "<EMAIL_RIFERIMENTO>";
-const RAGIONE_SOCIALE = "<RAGIONE_SOCIALE>";
-const COMUNE = "<COMUNE>";
-const PROVINCIA = "<PROVINCIA>";
-const CAP = "<CAP>";
-const PIVA = "<PIVA>";
-const DESCRIZIONE = "<DESCRIZIONE>";
-const TABELLA = "<TABELLA>";
+const INVOICEROW_START_COL = 2;
+const INVOICEROW_COLS_NR = 13;
+// STILI TABELLA
+const headerStyle: any = {};
+headerStyle[DocumentApp.Attribute.BOLD] = true;
+const cellStyle: any = {};
+cellStyle[DocumentApp.Attribute.BOLD] = false;
+cellStyle[DocumentApp.Attribute.UNDERLINE] = false;
+const amountCellStyle: any = {};
+amountCellStyle[DocumentApp.Attribute.BOLD] = false;
+amountCellStyle[DocumentApp.Attribute.ITALIC] = false;
+const slantedStyle: any = {};
+slantedStyle[DocumentApp.Attribute.STRIKETHROUGH] = true;
+slantedStyle[DocumentApp.Attribute.BOLD] = false;
+slantedStyle[DocumentApp.Attribute.ITALIC] = true;
+const paraStyle: any = {};
+paraStyle[DocumentApp.Attribute.SPACING_AFTER] = 0;
+paraStyle[DocumentApp.Attribute.LINE_SPACING] = 1;
+paraStyle[DocumentApp.Attribute.BOLD] = false;
+const pFooterStyle: any = {};
+pFooterStyle[DocumentApp.Attribute.SPACING_AFTER] = 0;
+pFooterStyle[DocumentApp.Attribute.LINE_SPACING] = 1;
+pFooterStyle[DocumentApp.Attribute.BOLD] = true;
+const footerStyle: any = {};
+footerStyle[DocumentApp.Attribute.STRIKETHROUGH] = false;
+footerStyle[DocumentApp.Attribute.BACKGROUND_COLOR] = "#f3f3f3";
 
 /** Inizializzazione e installazione */
 function onInstall(e: any) {
@@ -61,8 +80,11 @@ function showModalFatture() {
         .setHeight(modalHeight);
     //SpreadsheetApp.getUi().showModalDialog(template, 'Crea nuova Fattura');
 }
+
+/** Crea fattura dalle righe selezionate  */
 function createInvoiceFromSelection() {
     var activeSheet = SpreadsheetApp.getActiveSheet();
+    var sheetName = activeSheet.getName();
     var selection = activeSheet.getSelection();
     Logger.log('Current Cell: ' + selection.getCurrentCell().getA1Notation());
     Logger.log('Active Range: ' + selection.getActiveRange().getA1Notation());
@@ -75,12 +97,14 @@ function createInvoiceFromSelection() {
 }
 
 /** Crea nuova fattura */
-function CreaFattura(datiInput: DatoFattura) {
+function CreaFattura(datiInput: InvoiceItem) {
+    var activeSheet = SpreadsheetApp.getActiveSheet();
+    var sheetName = activeSheet.getName();
     Logger.log("Leggo i dati");
-    const currentOrder = LeggiDati(firstRow, lastRow);
+    const currentInvoice = LeggiDati(firstRow, lastRow);
     Logger.log("Esco");
     // return; 
-    Logger.log("CreaFAttura: " + JSON.stringify(datiInput));
+    //Logger.log("CreaFAttura: " + JSON.stringify(datiInput));
     const templateId = invoiceTemplateID;
     const templateDoc = DriveApp.getFileById(templateId);
     const date = new Date();
@@ -89,16 +113,16 @@ function CreaFattura(datiInput: DatoFattura) {
     const year = date.getFullYear();
     const dateString = "" + day + "/" + month + "/" + year;
     Logger.log("Data corrente: " + dateString);
-    const orderFullName = datiInput.orderName;
-    const orderAddress = datiInput.orderAddress;
+    const orderFullName = sheetName + GetDateString();
+    const orderAddress = currentInvoice[0].address;
     // Crea un nuovo documento dal template e sostituisce i dati
     const newDoc = templateDoc.makeCopy(orderFullName);
     const newDocId = newDoc.getId();
     const file = SpreadsheetApp.openById(newDocId);
     const sheet = file.getSheets()[0];
 
-    for (let index = 0; index < currentOrder.length; index++) {
-        const currItem = currentOrder[index];
+    for (let index = 0; index < currentInvoice.length; index++) {
+        const currItem = currentInvoice[index];
         Logger.log("Inserisco riga " + index + ": " + JSON.stringify(currItem));
         sheet.insertRowAfter(21);
         sheet.getRange(iRow, 11, 1, 1)//(start row, start column, number of rows, number of columns
@@ -107,7 +131,7 @@ function CreaFattura(datiInput: DatoFattura) {
         sheet.getRange(iRow, 2, 1, 2).merge();
         sheet.getRange(iRow, 1, 1, 10)//(start row, start column, number of rows, number of columns
             .setValues([[
-                currentOrder.length - index,
+                currentInvoice.length - index,
                 currItem.itemCode,
                 "",
                 "",
@@ -124,16 +148,16 @@ function CreaFattura(datiInput: DatoFattura) {
     cellReplaceText(sheet, CELL_ORDER_NR_1, NR_ORDINE, orderFullName);
     cellReplaceText(sheet, CELL_ORDER_NR_2, NR_ORDINE, orderFullName);
     cellReplaceText(sheet, CELL_DATA_1, DATA, dateString);
-    const currData2Row = CELL_DATA_2_ROW + currentOrder.length + 1;
+    const currData2Row = CELL_DATA_2_ROW + currentInvoice.length + 1;
     cellReplaceText(sheet, CELL_DATA_2_COL + currData2Row, DATA, dateString);
 
     /** Numero di righe da sommare per totale (numero item + 1) */
-    const sumRows = currentOrder.length + 1;
+    const sumRows = currentInvoice.length + 1;
     /** Formula per totale */
-    sheet.getRange(iRow + currentOrder.length + 1, TOTAL_START_COL, 1, 1)//(start row, start column, number of rows, number of columns
+    sheet.getRange(iRow + currentInvoice.length + 1, TOTAL_START_COL, 1, 1)//(start row, start column, number of rows, number of columns
         .setFormulaR1C1("=sum(R[-" + sumRows + "]C[0]:R[-1]C[0])");
     /** Formula per Q.tà */
-    sheet.getRange(iRow + currentOrder.length + 1, QTY_START_COL, 1, 1)//(start row, start column, number of rows, number of columns
+    sheet.getRange(iRow + currentInvoice.length + 1, QTY_START_COL, 1, 1)//(start row, start column, number of rows, number of columns
         .setFormulaR1C1("=sum(R[-" + sumRows + "]C[0]:R[-1]C[0])");
     /** Elimino riga di esempio */
     sheet.deleteRow(EXAMPLE_ROW);
@@ -144,12 +168,13 @@ function CreaFattura(datiInput: DatoFattura) {
 /** legge dal foglio i dati della fattura */
 function LeggiDati(rigaIniziale: number, rigaFinale: number, updateStock: boolean = true) {
     /** Dati ordine corrente */
-    let currentOrder: ItemFattura[] = new Array();
+    let currentInvoice: InvoiceItem[] = new Array();
     Logger.log("Inizio LeggiDati");
-    const file = SpreadsheetApp.openById(listinoID);
+    const file = SpreadsheetApp.openById(invoicesFileID);
     const ss = file.getSheets()[0];
     for (let riga: number = rigaFinale; riga >= rigaIniziale; riga--) {
-        const rangeToCheck = ss.getRange(riga, ORDERROW_START_COL, 1, ORDERROW_COLS_NR); // 10 columns starting with column 2, so B-J range 
+        // 13 columns starting with column 2, so B-N range 
+        const rangeToCheck = ss.getRange(riga, INVOICEROW_START_COL, 1, INVOICEROW_COLS_NR); 
         const readValues = rangeToCheck.getValues();
         const isChecked = readValues[0][0];
         if (isChecked) {
@@ -167,26 +192,25 @@ function LeggiDati(rigaIniziale: number, rigaFinale: number, updateStock: boolea
             const itemOffer: number = parseInt(readValues[0][8].toString());
             const itemStock: number = parseInt(readValues[0][9].toString());
             Logger.log("Aggiungo item " + itemCode);
-            AggiungiItem(currentOrder, nrItems, itemCode, itemDescription, itemPrice, itemCessione, itemOffer, itemStock);
+            AggiungiItem(currentInvoice, nrItems, itemCode, itemDescription, itemPrice, itemCessione, itemOffer, itemStock);
             if (updateStock) {
                 AggiornaStock(ss, riga, nrItems);
             }
         }
     }
     Logger.log("***");
-    Logger.log(JSON.stringify(currentOrder));
+    Logger.log(JSON.stringify(currentInvoice));
     Logger.log("***");
     Logger.log("Fine LeggiDati");
 
-    return currentOrder;
+    return currentInvoice;
 }
 
 /* TOOLS */
-
 /** Aggiunge un item all'ordine */
-function AggiungiItem(currentOrder: ItemFattura[], nrItems: number, itemCode: string,
+function AggiungiItem(currentOrder: InvoiceItem[], nrItems: number, itemCode: string,
     itemDesc: string, itemPrice: number, itemCessione: number, itemOffer: number, itemStock: number) {
-    const item: ItemFattura = {
+    const item: InvoiceItem = {
         nrItems: nrItems,
         itemCode: itemCode,
         itemDesc: itemDesc,
@@ -199,7 +223,7 @@ function AggiungiItem(currentOrder: ItemFattura[], nrItems: number, itemCode: st
     Logger.log("Aggiunto item " + JSON.stringify(item));
 }
 
-/** sostituisce nel contenuto di una cella un template con un valore  */
+/** Sastituisce nel contenuto di una cella un template con un valore  */
 function cellReplaceText(sheet: GoogleAppsScript.Spreadsheet.Sheet, cell: string, template: string, replacement: string) {
     Logger.log("Sostituzione nella cella " + cell + " di " + template + " con " + replacement);
     const dateCell = sheet.getRange(cell);
@@ -250,25 +274,9 @@ declare interface Date {
     yyyymmdd(): string;
 }
 
-/** Interfaccia descrizione item offerta */
-interface DatoFattura {
-    orderName: string;
-    descrizione: string;
-    nomeRiferimento: string;
-    orderAddress: string;
-    orderFullName: string;
-    emailRiferimento: string;
-    ragioneSociale: string;
-    comune: string;
-    cap: string;
-    prov: string;
-    indirizzo: string;
-    pIva: string;
-    orderType: string;
-}
-
 /** Interfacci descrizione item ordine */
-interface ItemFattura {
+interface InvoiceItem {
+    address: string;
     nrItems: number;
     itemCode: string;
     itemDesc: string;
