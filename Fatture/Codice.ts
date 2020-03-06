9460e1be091bfb08a5d0c92f52da7b0292da1be7
f-leoni <f.leoni@alfabook.it>
2020-03-06 18:01:46 +0100

    Aggiunta gestione canali

--- Fatture/Codice.ts
+++ Fatture/Codice.ts
@@ -3,10 +3,12 @@
  */
 const debug = false;
 /** ID documenti  */
-const invoicesFileID = "1eqLHaMm9DFpofqGtETu8Mu7sUCak7plWHyRV37lbV0A";
-const invoiceTemplateID = "1i8VS32ksIrcUvJJOrn983xDgrE5qy9sbo4KfdPRODXs";
+const invoicesFileID = ReadConfigValue("ID file fatture"); //ID del file contenete le fatture
+const invoiceTemplateID = ReadConfigValue("ID template fattura"); //ID del template di richiesta fatturazione
+const invoiceFolderID = ReadConfigValue("ID cartella fatture"); //ID del template di richiesta fatturazione
+//Logger.log("Cartella Fatture ID["+invoiceFolderID+"]");
 /** Righe file fatture */
-const firstRow = parseInt(ReadConfigValue("Prima riga fatture");
+const firstRow = parseInt(ReadConfigValue("Prima riga fatture"));
 const lastRow = parseInt(ReadConfigValue("Ultima riga fatture"));
 const statusCol = parseInt(ReadConfigValue("Colonna stato fattura"));
 const iRow = parseInt(ReadConfigValue("Riga dati template"));
@@ -19,6 +21,7 @@ const NR_ORDINE = ReadConfigValue("Segnaposto Nr Ordine");
 const SALES_CODE = ReadConfigValue("Segnaposto Uff. Vendite"); //OLSE - OLSD
 const DATA = ReadConfigValue("Segnaposto Data");
 const EWBS = ReadConfigValue("Segnaposto EWBS"); //A200V01920C030120000_001 
+const CANALE = ReadConfigValue("Segnaposto Canale"); // Es: IT01/Z2
 // RIFERIMENTI CELLE SHEET RICHIESTA FATTURA
 const CELL_ADDRESS = ReadConfigValue("Template Cella indirizzo");
 const CELL_DATA_1 = ReadConfigValue("Template Cella data 1");
@@ -30,6 +33,7 @@ const CELL_NAME = ReadConfigValue("Template Cella Ragione Sociale");
 const CELL_SAPCODE = ReadConfigValue("Template Cella Codice SAP");
 const CELL_EWBS = ReadConfigValue("Template Cella EWBS");
 const CELL_SALESCODE = ReadConfigValue("Template Cella Ufficio Vendite");
+const CELL_CANALE = ReadConfigValue("Template Cella Canale");
 const TOTAL_START_COL = parseInt(ReadConfigValue("Colonna Totale"));
 const QTY_START_COL = parseInt(ReadConfigValue("Colonna Quantità"));
 const EXAMPLE_ROW = parseInt(ReadConfigValue("Riga Esempio"));
@@ -64,9 +68,9 @@ function CreateInvoiceFromSelection() {
         showOkPrompt("Non hai selezionato nessuna riga!");
         return;
     }
-    // return; 
     //Logger.log("CreaFattura: " + JSON.stringify(datiInput));
     const templateId = invoiceTemplateID;
+    const folderId = invoiceFolderID;
     const templateDoc = DriveApp.getFileById(templateId);
     const date = new Date();
     const month = date.getMonth() + 1;
@@ -74,7 +78,6 @@ function CreateInvoiceFromSelection() {
     const year = date.getFullYear();
     const dateString = "" + day + "/" + month + "/" + year;
     Logger.log("Data corrente: " + dateString);
-    //filename = "Ft_" + currentInvoices[0].productType + "_" + currentInvoices[0].shortname + "_" + currentInvoices[0].year + pad(currentInvoices[0].month,2); //GetDateString();
     const orderAddress = currentInvoices[0].address;
     const ragioneSociale = currentInvoices[0].name;
     const sapCode = currentInvoices[0].sapCode;
@@ -82,6 +85,7 @@ function CreateInvoiceFromSelection() {
     const ewbs = currentInvoices[0].ewbs;
     const salesCode = currentInvoices[0].salesCode;
     const orderNr = currentInvoices[0].orderNr;
+    const canaleString = currentInvoices[0].channel;
     // Crea un nuovo documento dal template e sostituisce i dati
     const newDoc = templateDoc.makeCopy(filename);
     const newDocId = newDoc.getId();
@@ -122,6 +126,7 @@ function CreateInvoiceFromSelection() {
     cellReplaceText(sheet, CELL_ORDER_NR_1, NR_ORDINE, filename);
     cellReplaceText(sheet, CELL_SALESCODE, SALES_CODE, salesCode);
     cellReplaceText(sheet, CELL_DATA_1, DATA, dateString);
+    cellReplaceText(sheet, CELL_CANALE, CANALE, canaleString);
     const currData2Row = CELL_DATA_2_ROW + currentInvoices.length + 1;
     cellReplaceText(sheet, CELL_DATA_2_COL + currData2Row, DATA, dateString);
 
@@ -135,8 +140,18 @@ function CreateInvoiceFromSelection() {
         .setFormulaR1C1("=sum(R[-" + sumRows + "]C[0]:R[-1]C[0])");
     /** Elimino riga di esempio */
     sheet.deleteRow(EXAMPLE_ROW);
-
     Logger.log("Segnaposto sostituiti");
+    Logger.log("Pulizia foglio");
+    if(showYesNoPrompt("Si vuole cancellare la selezione?", "Pulizia")){
+        clearSheet();
+    }
+
+    // Sposto la fattura nella cartella "Fatture" su Drive 
+    Logger.log("Sposto il file nella cartella 'Fatture'");
+    moveFiles(newDocId, folderId);
+
+    Logger.log("FINE");
+
 }
 
 /** legge dal foglio i dati della fattura */
@@ -188,8 +203,10 @@ function LeggiDati(rigaIniziale: number, rigaFinale: number, updateStatus: boole
             Logger.log("  itemProductCode: " + itemProductCode);
             const itemQty: number = readValues[0][15].toString();
             Logger.log("  itemQty: " + itemQty);
-            const itemPrice: number = parseFloat(readValues[0][16].toString());
-            Logger.log("  itemPrice: " + itemPrice);
+            const itemPrice: number = parseFloat(readValues[0][17].toString());
+            Logger.log("  itemPrice: " + readValues[0][17].toString());
+            const itemChannel: string = readValues[0][20].toString();
+            Logger.log("  itemChannel: " + itemChannel);
 
             Logger.log("Aggiungo item " + itemName);
             AddiItem(currentInvoice,
@@ -208,6 +225,7 @@ function LeggiDati(rigaIniziale: number, rigaFinale: number, updateStatus: boole
                 itemPrice,
                 itemProductType,
                 itemOrderNr,
+                itemChannel
             );
             // Define filename only once in each run
             if (filename == "") {   
@@ -228,9 +246,10 @@ function LeggiDati(rigaIniziale: number, rigaFinale: number, updateStatus: boole
 
 /* TOOLS */
 /** Aggiunge un item all'ordine */
-function AddiItem(currentInvoice: InvoiceItem[], itemYear: number, itemMonth: number, itemName: string, itemShortname: string, itemAddress: string,
-    itemSapCode: string, itemCigCode: string, itemEwbsCode: string, itemSalesCode: string, itemDescription: string,
-    itemProductCode: string, itemQty: number, itemPrice: number, itemProductType: string, itemOrderNr: string) {
+function AddiItem(currentInvoice: InvoiceItem[], itemYear: number, itemMonth: number, itemName: string, itemShortname: string, 
+    itemAddress: string,itemSapCode: string, itemCigCode: string, itemEwbsCode: string, itemSalesCode: string, itemDescription: string,
+    itemProductCode: string, itemQty: number, itemPrice: number, itemProductType: string, itemOrderNr: string, itemChannel: string
+    ) {
     const item: InvoiceItem = {
         name: itemName,
         shortname: itemShortname,
@@ -247,6 +266,7 @@ function AddiItem(currentInvoice: InvoiceItem[], itemYear: number, itemMonth: nu
         price: itemPrice,
         productType: itemProductType,
         orderNr: itemOrderNr,
+        channel: itemChannel,
     };
     currentInvoice.push(item);
     Logger.log("Aggiunto item " + JSON.stringify(item));
@@ -261,13 +281,14 @@ function ReadConfigValue(paramName: string) {
     var data = sheet.getDataRange().getValues();
 
     for (var i = 0; i < data.length; i++) {
-        if (data[i][A] == paramName) { //[1] because column B
-            Logger.log((i + 1))
+        if (data[i][A] == paramName) {      
             value = data[i][B];
+            //Logger.log("[" + data[i][A] + "]");     
+            break;
         }
     }
+    Logger.log(paramName + " = [" + value + "]");
     return value;
-
 }
 
 /** Sastituisce nel contenuto di una cella un template con un valore  */
@@ -298,12 +319,11 @@ function clearSheet() {
     const ss = file.getSheets()[0];
 
     ss.getRange(2, 2, lastRow).setValue(false);
-    //ss.getRange(2, 3, lastRow).setValue("");
-
 }
 
+/** Show a info prompt */
 function showOkPrompt(text: string, title: string = "Attenzione") {
-    var ui = SpreadsheetApp.getUi(); // Same variations.
+    var ui = SpreadsheetApp.getUi();
 
     var result = ui.alert(
         title,
@@ -313,11 +333,36 @@ function showOkPrompt(text: string, title: string = "Attenzione") {
     return;
 }
 
+/** Show a yes/no choice prompt */
+function showYesNoPrompt(text: string, title: string = "Attenzione") {
+    var ui = SpreadsheetApp.getUi(); 
+
+    var result = ui.alert(
+        title,
+        text,
+        ui.ButtonSet.YES_NO);
+    
+    if (result == ui.Button.YES) {
+        return true;
+    }
+    return false;
+}
+
+/** Apply zero padding */
 function pad(num, size) {
     var s = num + "";
     while (s.length < size) s = "0" + s;
     return s;
 }
+
+/** Move file to folder */
+function moveFiles(sourceFileId, targetFolderId) {
+    Logger.log("Sposto file ID[" + sourceFileId + "] nella cartella ID[" + targetFolderId + "]");
+    var file = DriveApp.getFileById(sourceFileId);
+    file.getParents().next().removeFile(file);
+    DriveApp.getFolderById(targetFolderId).addFile(file);
+  }
+  
 
 /* 
  * TIPI E INTERFACCE 
@@ -359,4 +404,5 @@ interface InvoiceItem {
     price: number;
     productType: string;
     orderNr: string;
+    channel: string;
 }
